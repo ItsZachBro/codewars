20ea99ecd18facbbd6d8d381f47b42bad698cf36
ItsZachBro <blewishboy@gmail.com>
2023-04-20 22:04:24 -0400

    2 more

---
+++ 8kyu/Basic Mathematical Operations.js
@@ -0,0 +1,9 @@
+function basicOp(operation, value1, value2)
+{
+  // Code
+switch(operation){
+  case '*': return value1 * value2;
+  case '/': return value1 / value2;
+  case '-': return value1 - value2;
+  case '+': return value1 + value2;  
+}}
