959f0a28201ca8085495766f26042f83a9392a2e
ItsZachBro <blewishboy@gmail.com>
2023-04-30 12:55:32 -0400

    stuff

--- 8kyu/Find out whether the shape is a cube.js
+++
@@ -1,8 +0,0 @@
-var cubeChecker = function(volume, side){
-  
-    if (volume <= 0 | side <= 0) return false
-    let answer = (side * side * side) 
-    if (answer == volume) return true
-    
-    else return false
-  }
