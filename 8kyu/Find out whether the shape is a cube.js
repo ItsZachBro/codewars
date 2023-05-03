006cf18a4f50fac3b1f2bb92bef9ae941627e7a8
ItsZachBro <blewishboy@gmail.com>
2023-05-02 22:00:01 -0400

    a fist full of katas

---
+++ 8kyu/Find out whether the shape is a cube.js
@@ -0,0 +1,10 @@
+var cubeChecker = function(volume, side){
+  
+    if (volume <= 0 | side <= 0) return false
+    
+    let answer = (side * side * side) 
+    
+    if (answer == volume) return true
+    
+    else return false
+  }
---
+++ 8kyu/Remove First and Last Character.js
@@ -0,0 +1,6 @@
+function removeChar(str){
+     return str.slice(1, -1)
+   };
+   //return STRing.SLICEingoff
+   //(both the first and the oposite of the first characters)
+   
