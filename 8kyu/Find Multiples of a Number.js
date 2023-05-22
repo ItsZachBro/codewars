57ff3bc207f282efce5fa8ba9c034c81a5cb64da
ItsZachBro <blewishboy@gmail.com>
2023-05-22 12:56:51 -0400

    iteration practice

---
+++ 8kyu/Find Multiples of a Number.js
@@ -0,0 +1,11 @@
+function findMultiples(num, limit) {
+    // array that stores the multiples
+    const multiples = []
+  
+    // iterate from num up to limit with a step of num
+    for (let i = num; i <= limit; i += num) {
+      multiples.push(i) // add the current number to multiples array
+    }
+  
+    return multiples
+  }
