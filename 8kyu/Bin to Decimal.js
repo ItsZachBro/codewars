0ef373c2230c7ad17e85900a08caec9a35ab6423
ItsZachBro <blewishboy@gmail.com>
2023-06-13 23:49:44 -0400

    bin there, done that

---
+++ 8kyu/Bin to Decimal.js
@@ -0,0 +1,5 @@
+function binToDec(bin) {
+    return parseInt(bin, 2)
+}
+// parseInt() function is used to convert the binary string to decimal
+// the number 2 as the second argument is used to indicate that the input is a binary number
