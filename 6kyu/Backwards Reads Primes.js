c189f700ee5beecdd340f09ac6147f5aaa2f19e3
ItsZachBro <blewishboy@gmail.com>
2023-06-15 02:06:07 -0400

    fixed errors from last push

--- 6kyu/Backwards Reads Primes.js
+++ 6kyu/Backwards Reads Primes.js
@@ -1,30 +1,25 @@
-function backwardsPrime(start, stop) {
-    const result = []
-    for (let num = start; 
-         num <= stop; num++) {
-      let isPrime = true
-      let reverseNum = parseInt(num.toString().split("").reverse().join(""), 10)
-  
-      // check if number are primes
-      for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) {
-          isPrime = false
-          break
-        }
-      }
-  
-      // check if reversed number are primes
-      for (let i = 2; i <= Math.sqrt(reverseNum); i++) {
-        if (reverseNum % i === 0) {
-          isPrime = false
-          break
-        }
-      }
-  
-      if (isPrime && num !== reverseNum) {
-        result.push(num)
-      }
-    }  
-    return result
+function rotate(str) {
+  // Check if the input string is empty
+  if (str === '') {
+    // If it's empty, return an empty array
+    return [];
   }
+
+  // Create an empty array to store the rotated strings
+  const rotations = [];
   
+  // Get the length of the input string
+  const length = str.length;
+
+  // Iterate over each character of the input string
+  for (let i = 0; i < length; i++) {
+    // Rotate the string by moving the first character to the end
+    str = str.slice(1) + str[0];
+    
+    // Add the rotated string to the rotations array
+    rotations.push(str);
+  }
+
+  // Return the array of rotated strings
+  return rotations;
+}
